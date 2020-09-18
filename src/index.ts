b928d49cf1f7b87e97374b63caf320632a5aebf9
Takuma HANATANI <mail@potato4d.me>
2020-09-19 01:14:28 +0900

    feat: Implement inject loading attribue logic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const TMP: any = null
-export default TMP
+import { Transformer } from 'unified'
+import { Node } from 'unist'
+import visit from 'unist-util-visit'
+import hast from 'hast'
+
+function lazyloadPlugin(): Transformer {
+  function visitor(el: hast.Element) {
+    if (el.tagName !== 'img') {
+      return
+    }
+    el.properties = {
+      ...(el.properties || {}),
+      loading: 'lazy'
+    }
+  }
+
+  function transformer(htmlAST: Node): Node {
+    visit<hast.Element>(htmlAST, 'element', visitor)
+    return htmlAST
+  }
+
+  return transformer
+}
+
+export default lazyloadPlugin
+module.exports = lazyloadPlugin
