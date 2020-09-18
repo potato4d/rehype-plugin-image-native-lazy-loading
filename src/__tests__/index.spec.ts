82227b98d95602a4aca396aedac9b8028b1f7fa9
Takuma HANATANI <mail@potato4d.me>
2020-09-19 01:43:48 +0900

    test: Setup continuous integration

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -8,23 +8,27 @@ import html from 'rehype-stringify'
 async function process(rawMarkdown: string) {
   return new Promise((resolve, reject) => {
     unified()
-    .use(markdown)
-    .use(remark2rehype)
-    .use(lazyLoadPlugin)
-    .use(html)
-    .process(rawMarkdown, (err, file) => {
-      if (err) {
-        return reject(err)
-      }
-      return resolve(file.toString())
-    })
+      .use(markdown)
+      .use(remark2rehype)
+      .use(lazyLoadPlugin)
+      .use(html)
+      .process(rawMarkdown, (err, file) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(file.toString())
+      })
   })
 }
 
 describe('index.ts', () => {
   test('変換後の <img> タグに対して loading="lazy" 属性が正しく付与されているか', () => {
-    const content = fs.readFileSync(`${__dirname}/fixtures/content.md`, { encoding: 'utf-8' })
-    const correct = fs.readFileSync(`${__dirname}/fixtures/correct.html`, { encoding: 'utf-8' })
+    const content = fs.readFileSync(`${__dirname}/fixtures/content.md`, {
+      encoding: 'utf-8'
+    })
+    const correct = fs.readFileSync(`${__dirname}/fixtures/correct.html`, {
+      encoding: 'utf-8'
+    })
     return process(content).then((result) => {
       expect(result + '\n').toBe(correct)
     })
